3be8b160a2d1b2128769a5281479eaf59967f4d8
Tony David <antoniod_98@hotmail.com>
2018-09-04 11:51:52 -0500

    Initial Commit

---
+++ Parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parser
+//
+//  Created by Tony David on 04/09/18.
+//  Copyright © 2018 Tony David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
